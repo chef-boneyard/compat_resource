1ecea6c6cfd305471ed2391c9bdb5547867c55cf
John Keiser <john@johnkeiser.com>
2015-10-02 11:02:46 -0700

    Add log_deprecation on platforms that don't have it

--- files/lib/chef_compat/monkeypatches/chef.rb
+++ files/lib/chef_compat/monkeypatches/chef.rb
@@ -1,3 +1,11 @@
+require 'chef/chef_class'
+
 class Chef
   NOT_PASSED = Object.new if !defined?(NOT_PASSED)
+  # Earlier versions of Chef didn't have this message
+  if !respond_to?(:log_deprecation)
+    def self.log_deprecation(message)
+      Chef::Log.warn(message)
+    end
+  end
 end
