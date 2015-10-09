17c1272dca11c78c03ab2000a622047693c359ce
John Keiser <john@johnkeiser.com>
2015-10-08 19:57:32 -0700

    Make a better deprecation stack

--- files/lib/chef_compat/monkeypatches/chef.rb
+++ files/lib/chef_compat/monkeypatches/chef.rb
@@ -1,9 +1,30 @@
 class Chef
   NOT_PASSED = Object.new if !defined?(NOT_PASSED)
   # Earlier versions of Chef didn't have this message
-  if !respond_to?(:log_deprecation)
-    def self.log_deprecation(message)
-      Chef::Log.warn(message)
+  module ChefCompatDeprecation
+    def log_deprecation(message, location=nil)
+      if superclass.respond_to?(:log_deprecation)
+        if !location
+          # Pick the first caller that is *not* part of the Chef or ChefCompat gem,
+          # that's the thing the user wrote.
+          chef_compat_gem_path = File.expand_path("../../..", __FILE__)
+          chef_gem_path = File.expand_path("../..",::Chef::Resource.instance_method(:initialize).source_location[0])
+          caller(0..10).each do |c|
+            if !c.start_with?(chef_gem_path) && !c.start_with?(chef_compat_gem_path)
+              location = c
+              break
+            end
+          end
+        end
+
+        super
+      else
+        Chef::Log.warn(message)
+      end
     end
+  end
+
+  class<<self
+    prepend ChefCompatDeprecation
   end
 end
