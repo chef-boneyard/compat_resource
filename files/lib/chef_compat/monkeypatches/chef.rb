4e440911dfa9b6f783c61275e78b6f17d5d2a7be
John Keiser <john@johnkeiser.com>
2015-11-04 09:09:52 -0800

    Fix log_deprecation for good (hopefully) with NoMethodError rescue

--- files/lib/chef_compat/monkeypatches/chef.rb
+++ files/lib/chef_compat/monkeypatches/chef.rb
@@ -3,22 +3,24 @@ class Chef
   # Earlier versions of Chef didn't have this message
   module ChefCompatDeprecation
     def log_deprecation(message, location=nil)
-      if method(:log_deprecation).super_method
-        if !location
-          # Pick the first caller that is *not* part of the Chef or ChefCompat gem,
-          # that's the thing the user wrote.
-          chef_compat_gem_path = File.expand_path("../../..", __FILE__)
-          chef_gem_path = File.expand_path("../..",::Chef::Resource.instance_method(:initialize).source_location[0])
-          caller(0..10).each do |c|
-            if !c.start_with?(chef_gem_path) && !c.start_with?(chef_compat_gem_path)
-              location = c
-              break
-            end
+      if !location
+        # Pick the first caller that is *not* part of the Chef or ChefCompat gem,
+        # that's the thing the user wrote.
+        chef_compat_gem_path = File.expand_path("../../..", __FILE__)
+        chef_gem_path = File.expand_path("../..",::Chef::Resource.instance_method(:initialize).source_location[0])
+        caller(0..10).each do |c|
+          if !c.start_with?(chef_gem_path) && !c.start_with?(chef_compat_gem_path)
+            new_location = c
+            break
           end
         end
+      end
 
+      begin
         super
-      else
+      # Bleagh. `super_method` doesn't exist on older rubies and I haven't
+      # figured out a way to check for its existence otherwise.
+      rescue NoMethodError
         Chef::Log.warn(message)
       end
     end
