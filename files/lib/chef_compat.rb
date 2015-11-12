5f6004c9e9ecd14848ea66dc94a1d75f3a9acd04
Lamont Granquist <lamont@scriptkiddie.org>
2015-11-12 12:32:01 -0800

    include more chef-12 class methods

--- files/lib/chef_compat.rb
+++ files/lib/chef_compat.rb
@@ -13,7 +13,19 @@ else
 
   class Chef
     class Resource
-      def self.action(*args, &block)
+      def self.property(args, &block)
+        raise_chef_11_error
+      end
+
+      def self.resource_name(args, &block)
+        raise_chef_11_error
+      end
+
+      def self.action(args, &block)
+        raise_chef_11_error
+      end
+
+      def self.raise_chef_11_error
         raise "This resource is written with Chef 12.5 custom resources, and requires at least Chef 12.0 used with the compat_resource cookbook, it will not work with Chef 11.x clients, and those users must pin their cookbooks to older versions or upgrade."
       end
     end
