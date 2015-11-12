4dc7047cf1290639511d13b7aedaad5cf9831b59
Lamont Granquist <lamont@scriptkiddie.org>
2015-11-12 10:52:39 -0800

    move to resource parsing exception
    this move the exception to resource parsing time.
    
    since cookbooks that depend on compat_resource are written in Chef 12.5
    style and all cookbooks that are synched have all their resources
    loaded, i think this won't fix anyone.  the major benefit of throwing
    the exception here is that users more clearly see which code is
    inherently not supportable on Chef 11 and might stop blaming the
    messenger.

--- files/lib/chef_compat.rb
+++ files/lib/chef_compat.rb
@@ -1,7 +1,22 @@
-require 'chef_compat/version'
-require 'chef_compat/resource'
-require 'chef_compat/property'
-require 'chef_compat/mixin/properties'
 
-module ChefCompat
+if Gem::Requirement.new(">= 12.0").satisfied_by?(Gem::Version.new(Chef::VERSION))
+
+  require 'chef_compat/version'
+  require 'chef_compat/resource'
+  require 'chef_compat/property'
+  require 'chef_compat/mixin/properties'
+
+  module ChefCompat
+  end
+
+else
+
+  class Chef
+    class Resource
+      def self.action(*args, &block)
+        raise "This resource is written with Chef 12.5 converged resources, and requires at least Chef 12.0 used with the compat_resource cookbook, it will not work with Chef 11.x clients, and those users must pin their cookbooks to older versions or upgrade."
+      end
+    end
+  end
+
 end
