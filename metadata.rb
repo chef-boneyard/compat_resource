45a1784f86e04171672495515af5a6c50a4b789b
Tim Smith <tsmith84@gmail.com>
2015-12-08 09:46:37 -0800

    Add additional metadata fields

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,12 @@
-name "compat_resource"
-description "Chef 12.5 Resources, For Chef 12.1-12.4"
+name 'compat_resource'
+maintainer 'John Keiser'
+maintainer_email 'jkeiser@chef.io'
+license 'Apache 2.0'
+description 'Chef 12.5 Resources, For Chef 12.1-12.4'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 require_relative 'files/lib/chef_compat/version'
 version ChefCompat::VERSION
 
-chef_version ">= 12.0" if respond_to?(:chef_version)
+chef_version '>= 12.0' if respond_to?(:chef_version)
+source_url 'https://github.com/chef-cookbooks/compat_resource' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/compat_resource/issues' if respond_to?(:issues_url)
