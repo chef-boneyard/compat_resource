8ed2c3d7ec46a6a47cd051dd21ef839aae757293
John Keiser <john@johnkeiser.com>
2015-12-08 11:27:58 -0800

    Add to_h fix

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,6 @@ description 'Chef 12.5 Resources, For Chef 12.1-12.4'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 require_relative 'files/lib/chef_compat/version'
 version ChefCompat::VERSION
-maintainer 'John Keiser'
-maintainer_email 'jkeiser@chef.io'
 
 chef_version '>= 12.0' if respond_to?(:chef_version)
 source_url 'https://github.com/chef-cookbooks/compat_resource' if respond_to?(:source_url)
