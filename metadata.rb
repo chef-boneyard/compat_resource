2d4e58421ddecaf725fa95fe1b178313d35c8479
Tim Smith <tsmith@chef.io>
2016-08-31 16:46:44 -0700

    We require Chef 12.1 not 12.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 version '12.14.0'
 
-chef_version '>= 12.0' if respond_to?(:chef_version)
+chef_version '>= 12.1' if respond_to?(:chef_version)
 source_url 'https://github.com/chef-cookbooks/compat_resource'
 issues_url 'https://github.com/chef-cookbooks/compat_resource/issues'
