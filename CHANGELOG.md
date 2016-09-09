ecd8e7b461713f46b2e6bba9a9365b171c803bcb
Tim Smith <tsmith@chef.io>
2016-09-09 14:07:39 -0700

    Release 12.14.2
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 # compat_resource Cookbook CHANGELOG
 This file is used to list changes made in each version of the compat_resource cookbook.
 
+## 12.14.2 (2016-09-09)
+
+- Improve documentation
+- keep ChefCompat::Resource defined even if we don't load
+
 ## 12.14.1 (2016-09-07)
 
 - add yum_repository resource from Chef 12.14
--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'Apache 2.0'
 description 'Brings the modern custom resource syntax to earlier Chef 12 releases prior to 12.5'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version '12.14.1'
+version '12.14.2'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
 source_url 'https://github.com/chef-cookbooks/compat_resource'
