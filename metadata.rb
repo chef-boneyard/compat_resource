f7f990a92020aa0e96a13fd68a48fde3a2c886c2
Tim Smith <tsmith@chef.io>
2017-04-17 15:46:56 -0700

    Release 12.19.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'Apache 2.0'
 description 'Backports functionality introduced in the latest chef-client releases to any chef-client from 12.1 onwards'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version '12.16.3'
+version '12.19.0'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
 source_url 'https://github.com/chef-cookbooks/compat_resource'
